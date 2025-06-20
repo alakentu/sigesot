280b8f8d38bcb275693930d5c238bad3ad1843b9
Gonzalo Meneses <alakentu2003@gmail.com>
2025-06-20 10:02:16 -0400

    Actualización del sistema
    Se actualizó todo el framework Codeigniter a la versión 4.6.1

--- app/Config/Cache.php
+++ app/Config/Cache.php
@@ -21,8 +21,7 @@ class Cache extends BaseConfig
      * The name of the preferred handler that should be used. If for some reason
      * it is not available, the $backupHandler will be used in its place.
      */
-    //public string $handler = 'file';
-    public string $handler = 'redis';
+    public string $handler = 'file';
 
     /**
      * --------------------------------------------------------------------------
@@ -109,6 +108,7 @@ class Cache extends BaseConfig
      * -------------------------------------------------------------------------
      * Redis settings
      * -------------------------------------------------------------------------
+     *
      * Your Redis server can be specified below, if you are using
      * the Redis or Predis drivers.
      *
